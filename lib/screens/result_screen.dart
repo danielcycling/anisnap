e814f73e7d7696038bf93c006d9bfb2cbd6086f4
daniel <kirhashitatsuki@gmail.com>
2025-04-05 15:52:27 +0900

    update commit!

--- lib/screens/result_screen.dart
+++ lib/screens/result_screen.dart
@@ -1,6 +1,8 @@
 import 'dart:io';
 import 'package:flutter/material.dart';
 import 'package:tflite_flutter/tflite_flutter.dart';
+import 'dart:typed_data';
+import 'package:image/image.dart' as img;
 
 class ResultScreen extends StatefulWidget {
   const ResultScreen({super.key});
@@ -10,6 +12,43 @@ class ResultScreen extends StatefulWidget {
 }
 
 class _ResultScreenState extends State<ResultScreen> {
+  Future<void> _runInference(File imageFile) async {
+    final rawBytes = await imageFile.readAsBytes();
+    final image = img.decodeImage(rawBytes)!;
+
+    final inputSize = 300;
+    final resized = img.copyResize(image, width: inputSize, height: inputSize);
+
+    var input = Float32List(1 * inputSize * inputSize * 3);
+    var buffer = input.buffer.asFloat32List();
+
+    int pixelIndex = 0;
+    for (var y = 0; y < inputSize; y++) {
+      for (var x = 0; x < inputSize; x++) {
+        final pixel = resized.getPixel(x, y);
+        buffer[pixelIndex++] = (img.getRed(pixel)) / 255.0;
+        buffer[pixelIndex++] = (img.getGreen(pixel)) / 255.0;
+        buffer[pixelIndex++] = (img.getBlue(pixel)) / 255.0;
+      }
+    }
+
+    var outputLocations = List.filled(1 * 10 * 4, 0.0).reshape([1, 10, 4]);
+    var outputClasses = List.filled(1 * 10 , 0.0).reshape([1, 10]);
+    var outputScores = List.filled(1 * 10, 0.0).reshape([1, 10]);
+    var numDetections = List.filled(1, 0.0).reshape([1]);
+
+    _interpreter.runForMultipleInputs([input],{
+      0: outputLocations,
+      1: outputClasses,
+      2: outputScores,
+      3: numDetections,
+    });
+
+    print('推論完了');
+    print('検出数: ${numDetections[0][0]}');
+    print('scores: ${outputScores[0]}');
+
+  }
   late Interpreter _interpreter;
 
   @override
